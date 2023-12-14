bd5c7ac343e00a7eb39bfc426e5dfebd1f26de7e
Samuel Laws <120589781+samueLaws@users.noreply.github.com>
2023-12-14 10:05:18 +0000

    Add files via upload

---
+++ Random Noise/Perlin Noise/loadCanvas.js
@@ -0,0 +1,5 @@
+const mainCanvas = document.getElementById("main_canvas");
+const ctx = mainCanvas.getContext("2d");
+
+ctx.fillStyle = "black";
+ctx.fillRect(0,0,mainCanvas.width,mainCanvas.height)
