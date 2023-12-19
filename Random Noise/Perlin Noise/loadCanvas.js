ba1c5680b4bab0420fbff66b275a49405884b2f2
Sam <Sam@laws-home.co.uk>
2023-12-19 20:52:00 +0000

    Broken smooth function

--- Random Noise/Perlin Noise/loadCanvas.js
+++ Random Noise/Perlin Noise/loadCanvas.js
@@ -26,10 +26,11 @@ vectorMatrix.forEachIndex(Vector.newXYUnitary);
 const valueRange = [-1,1];
 const paramRange = [[0,800],[0,800]];
 const vectorSpacing = [(paramRange[0][1]-paramRange[0][0])/n,(paramRange[1][1]-paramRange[1][0])/m];
+const halfDiagonalDistance = Math.sqrt(vectorSpacing[0]*vectorSpacing[0] + vectorSpacing[1]*vectorSpacing[1])/2;
 
 
 
-function findValueAt(x,y){
+function findNonInterpValueAt(x,y){
     let sqIndx = findIndexPos(x,y);
     let posWithinSquare = getLocalPos(x,y);
     let closestFourVerteces = [vectorMatrix.get(sqIndx[0],sqIndx[1]),vectorMatrix.get(sqIndx[0]+1,sqIndx[1]),vectorMatrix.get(sqIndx[0]+1,sqIndx[1]+1),vectorMatrix.get(sqIndx[0],sqIndx[1]+1)];
@@ -39,13 +40,29 @@ function findValueAt(x,y){
     let dotProducts = getDotProducts(positionFromCorners,closestFourVerteces); //the center vector is 
 
     let distributions = distribution_Closest(distances);
-
     let sum = 0;
     for(let i = 0; i < 4; i++){
         sum += dotProducts[i]*distributions[i];
     }
-    return sum;
+    return sum; //-1 to 1
 }
+function findSmoothValueAt(x,y){
+    let sqIndx = findIndexPos(x,y);
+    let posWithinSquare = getLocalPos(x,y);
+    let closestFourVerteces = [vectorMatrix.get(sqIndx[0],sqIndx[1]),vectorMatrix.get(sqIndx[0]+1,sqIndx[1]),vectorMatrix.get(sqIndx[0]+1,sqIndx[1]+1),vectorMatrix.get(sqIndx[0],sqIndx[1]+1)];
+    let positionFromCorners = getPosFromCorners(posWithinSquare);
+    //cw around the vertex
+    let distances = getDistanceFromEachCorner(positionFromCorners);
+    let dotProducts = getDotProducts(positionFromCorners,closestFourVerteces); //the center vector is 
+
+    let distributions = distribution_Smooth(posWithinSquare);
+    let sum = 0;
+    for(let i = 0; i < 4; i++){
+        sum += dotProducts[i]*distributions[i];
+    }
+    return sum; //-1 to 1
+}
+
 function getLocalPos(x,y){
     let arr = [];
     arr.length = 2;
@@ -57,9 +74,16 @@ function getPosFromCorners(pos){
     let arr = [];
     arr.length = 4;
     arr[0] = [pos[0],pos[1]];
-    arr[1] = [vectorSpacing[0]-pos[0],pos[1]];
-    arr[2] = [vectorSpacing[0]-pos[0],vectorSpacing[1]-pos[1]];
-    arr[3] = [pos[0],vectorSpacing[1]-pos[1]];
+    arr[1] = [pos[0]-vectorSpacing[0],pos[1]];
+    arr[2] = [pos[0]-vectorSpacing[0],pos[1]-vectorSpacing[1]];
+    arr[3] = [pos[0],pos[1]-vectorSpacing[1]];
+
+    //scale down by half the square size (max possible value), clamps to -1,1
+    //slower to do it here but better?
+    for(let i = 0; i < 4; i++){
+        arr[i][0] = arr[i][0]/halfDiagonalDistance;
+        arr[i][1] = arr[i][1]/halfDiagonalDistance;
+    }
     return arr;
 }
 function findIndexPos(x,y){
@@ -102,6 +126,28 @@ function distribution_Closest(distances){
     }
     return weights;
 }
+function distribution_Smooth(posWithinSquare){
+    //scale pos within square down to 0-1
+    let pos = [posWithinSquare[0]/vectorSpacing[0],posWithinSquare[1]/vectorSpacing[1]];
+
+    let weights = [];
+    weights.length = 4;
+    //linear transformation for each distance from weighting of 0-1,
+    //then scale down the weighting to limit sum to 1
+    weights[0] = (1-pos[0])*(1-pos[1]);
+    weights[1] = (pos[0])*(1-pos[1]);
+    weights[2] = (pos[0])*(pos[1]);
+    weights[3] = (1-pos[0])*(pos[1]);
+
+    let weightSum = 0;
+    for(let i = 0; i < 4; i++){
+        weightSum += weights[i];
+    }
+    for(let i = 0; i < 4; i++){
+        weights[i] = weights[i]/weightSum;
+    }
+    return weights;
+}
 function findIndexOfSmallestValue(array){ //non null array
     let smallestValue = array[0];
     let smallestIndex = 0;
@@ -117,11 +163,11 @@ function findIndexOfSmallestValue(array){ //non null array
 //get each pixel value,
 paintCanvas();
 function paintCanvas(){
-    let minW = 0;
-    let maxW = 0;
+    let minW = 128;
+    let maxW = 128;
     for(let i = 0; i < 800; i++){
         for(let j = 0; j < 800; j++){
-            let w = findValueAt(i,j)*4.5 + 128;//
+            let w = findSmoothValueAt(i,j)*128+128;// desired output is -1-1
             minW = Math.min(minW,w);
             maxW = Math.max(maxW,w);
             ctx.fillStyle = rgb(w,w,w);
