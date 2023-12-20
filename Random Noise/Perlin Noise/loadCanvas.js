bb5be263181264eef63b6339ddf3f6fd4fa80f88
Sam <Sam@laws-home.co.uk>
2023-12-20 01:08:51 +0000

    Revert
    lots of stuff undone, some has been corrected, setup for lerp has started.

--- Random Noise/Perlin Noise/loadCanvas.js
+++ Random Noise/Perlin Noise/loadCanvas.js
@@ -29,6 +29,7 @@ const vectorSpacing = [(paramRange[0][1]-paramRange[0][0])/n,(paramRange[1][1]-p
 const halfDiagonalDistance = Math.sqrt(vectorSpacing[0]*vectorSpacing[0] + vectorSpacing[1]*vectorSpacing[1])/2;
 
 
+//currently only pre-interp stage
 
 function findNonInterpValueAt(x,y){
     let sqIndx = findIndexPos(x,y);
@@ -46,28 +47,12 @@ function findNonInterpValueAt(x,y){
     }
     return sum; //-1 to 1
 }
-function findSmoothValueAt(x,y){
-    let sqIndx = findIndexPos(x,y);
-    let posWithinSquare = getLocalPos(x,y);
-    let closestFourVerteces = [vectorMatrix.get(sqIndx[0],sqIndx[1]),vectorMatrix.get(sqIndx[0]+1,sqIndx[1]),vectorMatrix.get(sqIndx[0]+1,sqIndx[1]+1),vectorMatrix.get(sqIndx[0],sqIndx[1]+1)];
-    let positionFromCorners = getPosFromCorners(posWithinSquare);
-    //cw around the vertex
-    let distances = getDistanceFromEachCorner(positionFromCorners);
-    let dotProducts = getDotProducts(positionFromCorners,closestFourVerteces); //the center vector is 
-
-    let distributions = distribution_Smooth(posWithinSquare);
-    let sum = 0;
-    for(let i = 0; i < 4; i++){
-        sum += dotProducts[i]*distributions[i];
-    }
-    return sum; //-1 to 1
-}
 
 function getLocalPos(x,y){
     let arr = [];
     arr.length = 2;
-    arr[0] = (x-paramRange[0][0]) % vectorSpacing[0];
-    arr[1] = (y-paramRange[1][0]) % vectorSpacing[1];
+    arr[0] = (x-paramRange[0][0])%vectorSpacing[0];
+    arr[1] = (y-paramRange[1][0])%vectorSpacing[1];
     return arr;
 }
 function getPosFromCorners(pos){
@@ -81,8 +66,8 @@ function getPosFromCorners(pos){
     //scale down by half the square size (max possible value), clamps to -1,1
     //slower to do it here but better?
     for(let i = 0; i < 4; i++){
-        arr[i][0] = arr[i][0]/halfDiagonalDistance;
-        arr[i][1] = arr[i][1]/halfDiagonalDistance;
+        arr[i][0] = arr[i][0]/(halfDiagonalDistance*Math.SQRT2);
+        arr[i][1] = arr[i][1]/(halfDiagonalDistance*Math.SQRT2);
     }
     return arr;
 }
@@ -106,7 +91,7 @@ function getDotProducts(vectorsFromEach,fourVectors){
     let products = [];
     products.length = 4;
     for(let i = 0; i < 4; i++){
-        products[i] = vectorsFromEach[i][0]*fourVectors[i].x + vectorsFromEach[i][1]*fourVectors[i].y; //vectors form each are not scaled down to unitary, div each by the diagonal length of a square
+        products[i] = (vectorsFromEach[i][0]*fourVectors[i].x + vectorsFromEach[i][1]*fourVectors[i].y); //vectors form each are not scaled down to unitary, div each by the diagonal length of a square
     }
     return products;
 }
@@ -126,28 +111,6 @@ function distribution_Closest(distances){
     }
     return weights;
 }
-function distribution_Smooth(posWithinSquare){
-    //scale pos within square down to 0-1
-    let pos = [posWithinSquare[0]/vectorSpacing[0],posWithinSquare[1]/vectorSpacing[1]];
-
-    let weights = [];
-    weights.length = 4;
-    //linear transformation for each distance from weighting of 0-1,
-    //then scale down the weighting to limit sum to 1
-    weights[0] = (1-pos[0])*(1-pos[1]);
-    weights[1] = (pos[0])*(1-pos[1]);
-    weights[2] = (pos[0])*(pos[1]);
-    weights[3] = (1-pos[0])*(pos[1]);
-
-    let weightSum = 0;
-    for(let i = 0; i < 4; i++){
-        weightSum += weights[i];
-    }
-    for(let i = 0; i < 4; i++){
-        weights[i] = weights[i]/weightSum;
-    }
-    return weights;
-}
 function findIndexOfSmallestValue(array){ //non null array
     let smallestValue = array[0];
     let smallestIndex = 0;
@@ -160,6 +123,39 @@ function findIndexOfSmallestValue(array){ //non null array
     return smallestIndex;
 }
 
+const pwPoints = [[0,0],[0.2,0.05],[0.45,0.3],[0.5,0.5],[0.55,0.7],[0.8,0.95],[1,1]];
+//sigmoid approximation
+function pwdSmoothingFunctionLinear(x){
+    for(let i = 0; i < pwPoints.length-1; i++){
+        let pA = pwPoints[i];
+        let pB = pwPoints[i+1];
+        if(pA[0] <= x && x < pB[0]){
+            return pB[1]*(x-pA[0])/(pB[0]-pA[0]) + pA[1]*(x-pB[0])/(pA[0]-pB[0]); 
+        }
+    }
+    if(x < 0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+function combineLinearFunction(posA,dotProds){
+    let pos = [posA[0]/vectorSpacing[0],posA[1]/vectorSpacing[1]];
+    let pX = pwdS(pos[0]);
+    let pY = pwdS(pos[1]);
+    let sum = 0;
+    sum += dotProds[0]*pwdS(1-pX)*pwdS(1-pY);
+    sum += dotProds[1]*pwdS(1-pX)*pwdS(pY);
+    sum += dotProds[2]*pwdS(pX)*pwdS(pY);
+    sum += dotProds[3]*pwdS(pX)*pwdS(1-pY);
+    return sum;
+}
+function pwdS(x){
+    return pwdSmoothingFunctionLinear(x);
+}
+
+
 //get each pixel value,
 paintCanvas();
 function paintCanvas(){
@@ -167,7 +163,7 @@ function paintCanvas(){
     let maxW = 128;
     for(let i = 0; i < 800; i++){
         for(let j = 0; j < 800; j++){
-            let w = findSmoothValueAt(i,j)*128+128;// desired output is -1-1
+            let w = findNonInterpValueAt(i,j)*128+128;// desired output is -1-1
             minW = Math.min(minW,w);
             maxW = Math.max(maxW,w);
             ctx.fillStyle = rgb(w,w,w);
